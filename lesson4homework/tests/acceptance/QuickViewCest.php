71b2d3a1a9d13b15c089988196f1ef828a96ce11
AiasemKoshimbay <160107038@stu.sdu.edu.kz>
2021-05-26 00:44:40 +0600

    added seletors for searchcard function

--- lesson4homework/tests/acceptance/QuickViewCest.php
+++ lesson4homework/tests/acceptance/QuickViewCest.php
@@ -7,14 +7,18 @@ class QuickViewCest
      */
     public function checkCardByQuickView(AcceptanceTester $I)
     {
+        $cardXpath = '//*[@id="homefeatured"]/li[2]';
+        $quickViewCSS = '#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view >span';
+        $iFrameBoxXpath = '//*[@class="fancybox-iframe"]';
+
         $I->maximizeWindow();
         $I ->amOnPage('');
-        $I ->waitForElement('#homefeatured > li:nth-child(2) > div > div.left-block > div');
-        $I ->moveMouseOver('#homefeatured > li:nth-child(2) > div > div.left-block > div');
+        $I ->waitForElement('//*[@id="homefeatured"]/li[2]');
+        $I ->moveMouseOver('//*[@id="homefeatured"]/li[2]');
         $I ->waitForElement('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view > span');
         $I ->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view > span');
         $I ->wait(5);
-        $I ->switchToIFrame('.fancybox-iframe');
+        $I ->switchToIFrame('//*[@class="fancybox-iframe"]');
         $I->see('Blouse');
         
     }
